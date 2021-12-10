b7bff7bc499df9b35b921b88afde65fea551dd06
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-12-10 14:26:21 +0100

    Add doc string to the dataset.

--- src/datasets.py
+++ src/datasets.py
@@ -37,8 +37,17 @@ from torchvision import transforms as T
 
 
 class UADetracContextDetectionDataset(torch.utils.data.Dataset):
+    """UA-DETRAC detection dataset with the additional capability of adding
+    contextual images. If no context is specified, this class produces a dataset
+    representation that is tantamount to that which torchvision object detection
+    modules expect.
+    """
+
     @dataclasses.dataclass(frozen=True)
     class _SeqBoxesIndex:
+        """An auxiliary classs to store sequence index and image index within
+        that sequence.
+        """
         seq_idx: int
         img_idx: int
     
@@ -53,6 +62,27 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
         group_horizontal_flip=None,
         transforms=None
     ):
+        """Constructor.
+
+        Args:
+            root_path (str): Path to the UA-DETRAC dataset.
+            subset (str, optional): Data subset ('train' or 'test').
+                Defaults to 'train'.
+            past_context (int, optional): A non-negative integer specifying the
+                number of frames in the past. Defaults to 0.
+            future_context (int, optional): A non-negative integer specifying
+                the number of frames in the future. Defaults to 0.
+            context_stride (int, optional): A positive integer representing the
+                stride when traversing the past as well as future contextual
+                frames. Defaults to 1.
+            group_horizontal_flip (float, optional): If set, it should be a
+                float number in the <0, 1> interval representing the probability
+                of horizontally flipping all the images within a single group,
+                i.e., the center + the contextual images. Defaults to None.
+            transforms (Callable, optional): Transformation to apply to
+                individual frames. Beware that if context is required, some
+                transformations may be nonsensical. Defaults to None.
+        """
         self._context_rel_idxs = _calc_context_rel_idxs(
             past_context, future_context, context_stride
         )
@@ -67,6 +97,19 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
         self._init_data_indices(root_path, subset)
 
     def __getitem__(self, idx):
+        """Retrieves a random sample from the dataset.
+
+        Args:
+            idx (int): Data sample index.
+
+        Returns:
+            Tuple[torch.Tensor, Dict[str, List[torch.Tensor]]]: Returns a data
+            sample consisting of a center image in a tensor format, and target
+            specification as a dictionary with the following content:
+                'boxes': A Nx4 tensor of boxes in xyxy format.
+                'labels': A N, tensor of labels (0 indicates background).
+                'context_images': A list of tensors of contextual images.
+        """
         seq_box_idx = self._global_to_local_seq_img_idxs[idx]
         seq_idx, center_img_idx = seq_box_idx.seq_idx, seq_box_idx.img_idx
 
@@ -115,6 +158,14 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
         return len(self._global_to_local_seq_img_idxs)
 
     def _init_data_indices(self, root_path, subset):
+        """Initializes data indices to faster access. It reads image (frame)
+        file names as well as their corresponding bounding boxes in a tensor
+        format for faster access later on.
+
+        Args:
+            root_path ([type]): UA-DETRAC dataset root path.
+            subset ([type]): Whether to read 'train' or 'test' data subset.
+        """
         imgs_dir, annos_dir = self._deduce_imgs_and_annos_paths(
             root_path, subset
         )
@@ -177,6 +228,16 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
     
     @staticmethod
     def _iter_seq_img_file_paths(seq_dir):
+        """Iterates over image file names for a specific sequence from the
+        UA-DETRAC dataset.
+
+        Args:
+            seq_dir (pathlib.Path): Sequence directory path.
+
+        Yields:
+            Tuple[int, str]: Yield tuples containing image (frame) number and
+            the corresponding file path.
+        """
         for img_file in seq_dir.iterdir():
             img_num = int(img_file.stem[-5:])
             img_file_path = str(img_file)
@@ -185,6 +246,17 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
 
     @staticmethod
     def _iter_seq_boxes(xml_file_path):
+        """Iterates over a sequence of bounding boxes contained within a
+        specific XML file corresponding to some sequence from the UA-DETRAC
+        dataset.
+
+        Args:
+            xml_file_path (str): Sequence specification XML file path.
+
+        Yields:
+            Tuple[int, List[torch.Tensor]]: A tuple containing the frame number
+            and the list of bounding boxes in a tensor xyxy format.
+        """
         tree = ElementTree.parse(xml_file_path)
         root = tree.getroot()
 
@@ -208,12 +280,7 @@ class UADetracContextDetectionDataset(torch.utils.data.Dataset):
 
 
 def collate_context_imgs_batch(batch):
-    imgs, targets = [], []
-    
-    for imgs_item, targets_item in batch:
-        imgs.append(imgs_item)    
-        targets.append(targets_item)
-
+    imgs, targets = map(list, zip(*batch))
     return imgs, targets
 
 
